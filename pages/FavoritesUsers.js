f1dacc86a81ad1746dfdf0fe08e871e415d1363b
artium_96 <artium20@gmail.com>
2023-05-14 18:05:25 +0300

    create loading in favorite user page

--- pages/FavoritesUsers.js
+++ pages/FavoritesUsers.js
@@ -1,8 +1,13 @@
 import dynamic from 'next/dynamic'
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 const FavoritesUserId = dynamic(() => import('../components/FavoritesUserId'), {
-    ssr: false,
-})
+  loading: () => <div style={{ color: "gray", display: "flex", justifyContent: "center", marginBottom: "20%", marginTop: "20%" }}>
+    <CircularProgress color="inherit" size={60} />
+  </div>,
+});
+
 
 
 function FavoritesUsers() {
