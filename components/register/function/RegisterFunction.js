0cef3f35d7aeca55c58a6ac1c81d230f9c0060f2
artium_96 <artium20@gmail.com>
2024-01-05 19:53:58 +0200

    bag

--- components/register/function/RegisterFunction.js
+++ components/register/function/RegisterFunction.js
@@ -5,7 +5,7 @@ import { addUser } from '../../../api-helpers/frontend/utils';
 
 export const registerNewUser = async (inputs, setShowAlertShowAlertHaveLoginInDatabase, setDisabledRegesterButton, setDisabledCloseButton, setShowAlertUserRegister) => {
 
-    const loginUser = inputs.login;
+    let loginUser = inputs.login;
 
     registerCheckIfHaveThisUserInDataBase(loginUser)
         .then((dataCategory) => { dataCategory ? loginUser="" : !dataCategory?alert("no have"):null }
