db6fe4b9a54c60e5a36dce3f2c376e944c42bed9
artium_96 <artium20@gmail.com>
2024-01-05 19:53:00 +0200

    bag

--- components/register/function/RegisterFunction.js
+++ components/register/function/RegisterFunction.js
@@ -8,7 +8,7 @@ export const registerNewUser = async (inputs, setShowAlertShowAlertHaveLoginInDa
     const loginUser = inputs.login;
 
     registerCheckIfHaveThisUserInDataBase(loginUser)
-        .then((dataCategory) => { dataCategory ? alert("have") : !dataCategory?alert("no have"):null }
+        .then((dataCategory) => { dataCategory ? loginUser="" : !dataCategory?alert("no have"):null }
         )
         .catch((err) => console.log(err));
 
