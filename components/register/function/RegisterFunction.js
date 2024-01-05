412dafbab2b50238408b7a2a1f67605ef1e8f8f2
artium_96 <artium20@gmail.com>
2024-01-05 19:32:47 +0200

    fix bag

--- components/register/function/RegisterFunction.js
+++ components/register/function/RegisterFunction.js
@@ -5,22 +5,22 @@ import { addUser } from '../../../api-helpers/frontend/utils';
 
 export const registerNewUser = async (inputs, setShowAlertShowAlertHaveLoginInDatabase, setDisabledRegesterButton, setDisabledCloseButton, setShowAlertUserRegister) => {
 
+alert(registerCheckIfHaveThisUserInDataBase(inputs.login))
+    // if (registerCheckIfHaveThisUserInDataBase(inputs.login)) {
+    //     setShowAlertShowAlertHaveLoginInDatabase(true);
+    //     setDisabledRegesterButton(false);
+    //     return;
+    // }
 
-    if (await registerCheckIfHaveThisUserInDataBase(inputs.login)) {
-        setShowAlertShowAlertHaveLoginInDatabase(true);
-        setDisabledRegesterButton(false);
-        return;
-    }
-
-    else {
-        setDisabledRegesterButton(true);
-        setDisabledCloseButton(true);
-        await addUser(inputs)
-            .then((value) => console.log(value))
-            .then(() => {
-                setShowAlertUserRegister(true);
-            })
-            .catch(err => console.log(err));
-    }
+    // else {
+    //     setDisabledRegesterButton(true);
+    //     setDisabledCloseButton(true);
+    //     await addUser(inputs)
+    //         .then((value) => console.log(value))
+    //         .then(() => {
+    //             setShowAlertUserRegister(true);
+    //         })
+    //         .catch(err => console.log(err));
+    // }
 
 }
