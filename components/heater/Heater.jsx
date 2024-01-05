1bdb934642a14c492f0909b03a16df6d35a6bc10
artium_96 <artium20@gmail.com>
2024-01-06 01:05:25 +0200

    fix bag

--- components/heater/Heater.jsx
+++ components/heater/Heater.jsx
@@ -336,8 +336,8 @@ const Header = () => {
                               mainPagesChooseResponsiveScreenUserConnect(
                                 page.id,
                                 history,
-                                ()=>handleShowModelProfileUser,
-                                ()=>setAnchorElUser
+                                handleShowModelProfileUser,
+                                setAnchorElUser
                               )
                             }
                           >
