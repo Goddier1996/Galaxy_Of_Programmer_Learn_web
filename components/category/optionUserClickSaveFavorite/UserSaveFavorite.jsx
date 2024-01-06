df41b06a87391aa93dc5741e9d2a250368648419
artium_96 <artium20@gmail.com>
2024-01-06 15:44:27 +0200

    bag

--- components/category/optionUserClickSaveFavorite/UserSaveFavorite.jsx
+++ components/category/optionUserClickSaveFavorite/UserSaveFavorite.jsx
@@ -7,7 +7,7 @@ import { SaveFavorite } from './function/SaveFavoride';
 
 
 
-const UserSaveFavorite = ({ favorite, title, type, idFavorite }) => {
+const UserSaveFavorite = ( favorite, title, type, idFavorite ) => {
 
 
     const [showAlertUserAddFavorite, setShowAlertUserAddFavorite] = useState(false);
