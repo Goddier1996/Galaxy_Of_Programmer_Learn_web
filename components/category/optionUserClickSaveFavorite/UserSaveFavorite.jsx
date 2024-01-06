0ea30d5c95222cd824a0863482b9c47fd478fdf7
artium_96 <artium20@gmail.com>
2024-01-06 14:55:57 +0200

    fix bag

--- components/category/optionUserClickSaveFavorite/UserSaveFavorite.jsx
+++ components/category/optionUserClickSaveFavorite/UserSaveFavorite.jsx
@@ -3,7 +3,6 @@ import { Snackbar, Alert } from '@mui/material';
 import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
 import styles from "../infoCategoryPage.module.css"
 import { addUserFavorite, checkIfUserHaveThisFavorite } from '../../../api-helpers/frontend/utils';
-import ShowAlertFavorite from './alerts/ShowAlertFavorite';
 
 
 
@@ -74,36 +73,94 @@ const UserSaveFavorite = ({ favorite, title, type, idFavorite }) => {
                 <BookmarkAddIcon onClick={SaveFavorite} className={styles.buttonSave} />
             </b>
 
+
             {showAlertUserAddFavorite && (
-        <ShowAlertFavorite
-          showAlert={showAlertUserAddFavorite}
-          setShow={() => setShowAlertUserAddFavorite}
-          text={null}
-          type={type}
-        />
-      )}
+                <Snackbar
+                    open={showAlertUserAddFavorite}
+                    autoHideDuration={3000}
+                    onClose={() => {
+                        setShowAlertUserAddFavorite(false)
+                    }}>
 
-      {showAlertUserNeedConnectToAddFavorite && (
-        <ShowAlertFavorite
-          showAlert={showAlertUserNeedConnectToAddFavorite}
-          setShow={() => setShowAlertUserNeedConnectToAddFavorite}
-          text={"Please SignIn Or Register"}
-          type={null}
-        />
-      )}
 
-      {showAlertUserHaveThisFavorite && (
-        <ShowAlertFavorite
-          showAlert={showAlertUserHaveThisFavorite}
-          setShow={() => setShowAlertUserHaveThisFavorite}
-          text={"You Have This Favorite"}
-          type={null}
-        />
-      )}
+                    <Alert
+                        variant="filled" severity="success"
+                        sx={{ width: '100%', fontSize: "17px", textAlign: "center" }}>
+                        favorite save {type}<br />
+                        {title}
+                    </Alert>
+
+                </Snackbar>
+            )}
+
+
+            {showAlertUserNeedConnectToAddFavorite && (
+                <Snackbar
+                    open={showAlertUserNeedConnectToAddFavorite}
+                    autoHideDuration={3000}
+                    onClose={() => {
+                        setShowAlertUserNeedConnectToAddFavorite(false)
+                    }}>
+
+
+                    <Alert
+                        variant="filled" severity="info"
+                        sx={{ width: '100%', fontSize: "17px", textAlign: "center" }}>
+                        Please SignIn Or Register <br />
+                        and you can Add This Favorite
+                    </Alert>
+
+                </Snackbar>
+            )}
+
+
+            {showAlertUserHaveThisFavorite && (
+                <Snackbar
+                    open={showAlertUserHaveThisFavorite}
+                    autoHideDuration={3000}
+                    onClose={() => {
+                        setShowAlertUserHaveThisFavorite(false)
+                    }}>
+
+
+                    <Alert
+                        variant="filled" severity="warning"
+                        sx={{ width: '100%', fontSize: "17px", textAlign: "center" }}>
+                        You Have This Favorite<br />
+                        Please Check in your Favorites
+                    </Alert>
+
+                </Snackbar>
+            )}
         </>
     )
 }
 
+// {showAlertUserAddFavorite && (
+//     <ShowAlertFavorite
+//       showAlert={showAlertUserAddFavorite}
+//       setShow={() => setShowAlertUserAddFavorite}
+//       text={null}
+//       type={type}
+//     />
+//   )}
 
+//   {showAlertUserNeedConnectToAddFavorite && (
+//     <ShowAlertFavorite
+//       showAlert={showAlertUserNeedConnectToAddFavorite}
+//       setShow={() => setShowAlertUserNeedConnectToAddFavorite}
+//       text={"Please SignIn Or Register"}
+//       type={null}
+//     />
+//   )}
+
+//   {showAlertUserHaveThisFavorite && (
+//     <ShowAlertFavorite
+//       showAlert={showAlertUserHaveThisFavorite}
+//       setShow={() => setShowAlertUserHaveThisFavorite}
+//       text={"You Have This Favorite"}
+//       type={null}
+//     />
+//   )}
 
 export default UserSaveFavorite
