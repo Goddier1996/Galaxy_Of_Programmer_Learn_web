d92a62602074e4a339d1dc754e4dd180f54cf0a5
artium_96 <artium20@gmail.com>
2024-01-06 14:22:40 +0200

    create component show videos to better

--- components/category/video/ShowVideos.jsx
+++ components/category/video/ShowVideos.jsx
@@ -1,68 +1,61 @@
-import React from 'react'
-import UserSaveFavorite from '../optionUserClickSaveFavorite/UserSaveFavorite';
-import styles from "../infoCategoryPage.module.css"
-import UserDeleteFavorite from '../../profileUser/UserDeleteFavorite';
+import React from "react";
+import UserSaveFavorite from "../optionUserClickSaveFavorite/UserSaveFavorite";
+import styles from "../infoCategoryPage.module.css";
+import UserDeleteFavorite from "../../profileUser/UserDeleteFavorite";
+
 
 
 const ShowVideos = ({ data, use }) => {
 
 
-    // show if howUse = userProfile , we show in profile , or info show in page info category
-    if (use == "info") {
-        var { _id, titleVideo, video, type } = data;
-    }
+  // show if howUse = userProfile , we show in profile , or info show in page info category
+  if (use == "info") {
+    var { _id, titleVideo, video, type } = data;
+  }
+  else if (use == "user") {
+    var { _id, title, favorite, type } = data;
+  }
 
-    else if (use == "user") {
-        var { _id, title, favorite, type } = data;
-    }
+  // switch (use) {
 
-    // switch (use) {
-        
-    //     case "userProfile":
-    //         favoriteSaveIdUserVideo(idCategoryVideo, { signal: controller.signal })
-    //         var { _id, title, favorite, type } = data;
-    //         break;
-          
-    //     case "info":
-    //         var { _id, titleVideo, video, type } = data;
-    //         break;
-          
-    //     default:
-    //         console.log("not have any data");
-    // }
+  //     case "userProfile":
+  //         favoriteSaveIdUserVideo(idCategoryVideo, { signal: controller.signal })
+  //         var { _id, title, favorite, type } = data;
+  //         break;
 
-    return (
-        <>
-            {use == "info" ?
-                <>
-                    <div className={styles.videoInfoCategory} key={_id}>
-                            <>
-                                <p>{titleVideo}
-                                    <UserSaveFavorite
-                                        favorite={video}
-                                        title={titleVideo}
-                                        type={type}
-                                        idFavorite={_id}
-                                    />
-                                </p>
-                                <iframe allowFullScreen='allowFullScreen' src={video} alt="video Learn" />
-                            </>         
-                    </div>
-                </>
-                :
-                use == "user" ?
-                    <div className={styles.videoInfoCategory} key={_id}>
-                            <>
-                                <p>{title}
-                                    <UserDeleteFavorite id={_id} />
-                                </p>
-                                <iframe allowFullScreen='allowFullScreen' src={favorite} alt="video Learn" />
-                            </>
-                            :                   
-                    </div> : ""
-            }
-        </>
-    )
-}
+  //     case "info":
+  //         var { _id, titleVideo, video, type } = data;
+  //         break;
+
+  //     default:
+  //         console.log("not have any data");
+  // }
+
+    
+  return (
+    <div className={styles.videoInfoCategory} key={_id}>
+        <p>
+          {use == "info" ? titleVideo : use == "user" ? title : null}
+          {use == "info" ? (
+            <UserSaveFavorite
+              favorite={video}
+              title={titleVideo}
+              type={type}
+              idFavorite={_id}
+            />
+          ) : use == "user" ? (
+            <UserDeleteFavorite id={_id} />
+          ) : null}
+        </p>
+
+        <iframe
+          allowFullScreen="allowFullScreen"
+          src={use == "info" ? video : use == "user" ? favorite : null}
+          alt="video Learn"
+        />
+    </div>
+  );
+};
+
 
 export default ShowVideos;
